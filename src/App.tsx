918388f1d10d3d9d32bc85816b12084b14f5e64b
chenyilong <1456014766@qq.com>
2020-11-25 20:19:08 +0800

    完成button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './styles/index.scss'
+import Button , {ButtonType, ButtonSize} from './components/Button/button'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <Button>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Hello</Button>
+        <Button btnType={ButtonType.Link} href="https://www.baidu.com">Hello</Button>
+        <Button btnType={ButtonType.Link} disabled={true} href="https://www.baidu.com">Hello</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export enum ButtonSize {
+  Large = 'lg',
+  Small = 'sm'
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link',
+  Href = 'href',
+}
+
+interface BaseButtonProps {
+  className?: string;
+  disabled ?: boolean;
+  size ?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+
+// 这里是在 react 中拿到了button和a原本的标签定义的属性
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+
+// 设置之间的属性全部为可读的
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const {className, btnType, disabled, size, children, href, ...restProps} = props
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (bt,nType === ButtonType.Link) && disabled
+  })
+
+  if(btnType === ButtonType.Link && href) {
+    return (
+      <a
+        className={classes}
+        href={href}
+        {...restProps}
+      >
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <button
+        className={classes}
+        disabled={disabled}
+        {...restProps}
+      >
+        {children}
+      </button>
+    )
+  }
+
+}
+
+Button.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
+}
+
+export default Button
